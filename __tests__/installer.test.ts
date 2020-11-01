af20c6e1f71753f044065b73e6caf72e04883fcf
CrazyMax <crazy-max@users.noreply.github.com>
2020-11-01 23:00:22 +0100

    Fix tests

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -5,12 +5,12 @@ describe('installer', () => {
   it('acquires v0.3.1 version of xgo', async () => {
     const xgo = await installer.getXgo('v0.3.1');
     console.log(xgo);
-    expect(fs.existsSync(xgo)).toBe(true);
+    expect(fs.existsSync(xgo.path)).toBe(true);
   }, 100000);
 
   it('acquires latest version of xgo', async () => {
     const xgo = await installer.getXgo('latest');
     console.log(xgo);
-    expect(fs.existsSync(xgo)).toBe(true);
+    expect(fs.existsSync(xgo.path)).toBe(true);
   }, 100000);
 });
