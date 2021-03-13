072c1824a1b8ecb5b7d9009e46d2cf10f4f98349
CrazyMax <crazy-max@users.noreply.github.com>
2021-03-13 02:35:39 +0100

    Fix tests

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -2,8 +2,8 @@ import fs = require('fs');
 import * as installer from '../src/installer';
 
 describe('installer', () => {
-  it('acquires v0.3.1 version of xgo', async () => {
-    const xgo = await installer.getXgo('v0.3.1');
+  it('acquires v0.6.0 version of xgo', async () => {
+    const xgo = await installer.getXgo('v0.6.0');
     console.log(xgo);
     expect(fs.existsSync(xgo.path)).toBe(true);
   }, 100000);
