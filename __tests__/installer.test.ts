78fc1f82fdf3b17db147edc7fbdcd2ee2b674687
CrazyMax <crazy-max@users.noreply.github.com>
2022-04-25 01:32:48 +0200

    update dev dependencies and workflow

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -1,16 +1,15 @@
-import fs = require('fs');
+import {describe, expect, it} from '@jest/globals';
+import * as fs from 'fs';
 import * as installer from '../src/installer';
 
 describe('installer', () => {
   it('acquires v0.6.0 version of xgo', async () => {
     const xgo = await installer.getXgo('v0.6.0');
-    console.log(xgo);
     expect(fs.existsSync(xgo.path)).toBe(true);
   }, 100000);
 
   it('acquires latest version of xgo', async () => {
     const xgo = await installer.getXgo('latest');
-    console.log(xgo);
     expect(fs.existsSync(xgo.path)).toBe(true);
   }, 100000);
 });
