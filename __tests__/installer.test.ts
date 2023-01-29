9cd18baa40d88446c1ba3de181d952e7c1e37720
CrazyMax <crazy-max@users.noreply.github.com>
2023-01-29 03:05:19 +0100

    check latest and tagged releases using releases-json

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -2,6 +2,26 @@ import {describe, expect, it} from '@jest/globals';
 import * as fs from 'fs';
 import * as installer from '../src/installer';
 
+describe('getRelease', () => {
+  it('returns latest buildx GitHub release', async () => {
+    const release = await installer.getRelease('latest');
+    expect(release).not.toBeNull();
+    expect(release?.tag_name).not.toEqual('');
+  });
+
+  it('returns v0.24.0 buildx GitHub release', async () => {
+    const release = await installer.getRelease('v0.24.0');
+    expect(release).not.toBeNull();
+    expect(release?.id).toEqual(88251102);
+    expect(release?.tag_name).toEqual('v0.24.0');
+    expect(release?.html_url).toEqual('https://github.com/crazy-max/xgo/releases/tag/v0.24.0');
+  });
+
+  it('unknown release', async () => {
+    await expect(installer.getRelease('foo')).rejects.toThrowError(new Error('Cannot find Xgo release foo in https://raw.githubusercontent.com/crazy-max/ghaction-xgo/master/.github/xgo-releases.json'));
+  });
+});
+
 describe('installer', () => {
   it('acquires v0.6.0 version of xgo', async () => {
     const xgo = await installer.getXgo('v0.6.0');
